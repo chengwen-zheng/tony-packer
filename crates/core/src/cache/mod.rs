57d87b7d879e823ae7a5429efe77d9f52bc00bb0
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-07-09 10:20:50 +0800

    feat: add CacheManager struct

--- crates/core/src/cache/mod.rs
+++ crates/core/src/cache/mod.rs
@@ -1,2 +1,24 @@
+use cache_store::CacheStore;
+use module_cache::ModuleCacheManager;
+use tokio::sync::Mutex;
+
 pub mod cache_store;
 pub mod module_cache;
+
+pub struct CacheManager {
+    pub module_cache: ModuleCacheManager,
+
+    pub lazy_compile_store: CacheStore,
+
+    pub custom: CacheStore,
+
+    pub lock: Mutex<bool>,
+}
+
+// impl CacheManager {
+//     pub fn new() -> Self {
+//         Self {
+
+//         }
+//     }
+// }
